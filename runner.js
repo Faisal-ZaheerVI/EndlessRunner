9d4f5a266fb077680aad3ca3775b6f9c5853d206
Greg Gorlen <gsgorlen@gmail.com>
2016-12-08 21:04:21 -0800

    post-class update, fix setInterval and refactor pause

--- runner.js
+++ runner.js
@@ -3,6 +3,14 @@
  * - myGamePiece = player
  */
 
+// Background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "imgs/backdrop.png";
+
 // Creates Game Canvas with properties
 var game = {
     canvas : document.createElement("canvas"),
@@ -13,17 +21,22 @@ var game = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         //this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
+		/*if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+		}*/
     },
     
     // Clears canvas
     clear : function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.context.drawImage(bgImage, 0, 0, this.canvas.width, this.canvas.height)
     }
-}; // end Game object
+}; // End Game object
 
 // Keyboard handler
 var kbd = function () {
   this.up = false;
+  this.p = true;
 };
 
 // add keyevent listener to track arrow key actions
@@ -37,14 +50,14 @@ document.addEventListener("keydown", function (e) {
   else if (e.keyCode === 37 || e.keyCode === 65) {
     kbd.left = true;
   }
-  else if (e.keyCode === 27) {
-       paused = !paused;
+  else if (e.keyCode === 80) {
+    kbd.p = true;
   }
 }, false);
 
 document.addEventListener("keyup", function (e) {
   if (e) {
-    kbd.up = kbd.left = kbd.right = false;
+    kbd.up = kbd.left = kbd.right = kbd.p = false;
   }
 }, false);
 
@@ -53,22 +66,37 @@ game.start(); // canvas not created until this function is called
 // Constants
 var ROCK_BOTTOM = game.canvas.height;
 var GRID_SIZE = 10;
+
 var PLATFORM_HEIGHT = 10;
+var MAX_PLAT_HEIGHT = 0;
+var MIN_PLAT_HEIGHT = game.canvas.height - PLATFORM_HEIGHT;
+
+var MAX_PLAT_WIDTH = 75;
+var MIN_PLAT_WIDTH = 125;
 var PLATFORM_WIDTH = 100;
+
 var PLATFORM_SPEED = 3;
+
+// Initialization X and Y coordinates
 var INIT_X = game.canvas.width;
 var INIT_Y = game.canvas.height - PLATFORM_HEIGHT;
+var MIN_PLAT_Y = 160;
+var MAX_PLAT_Y = 220;
+
 //var PLAYER_IMG = "imgs/download.png"
 
-//var NUM_PLATFORMS = 3;
+var NUM_PLATFORMS = 1;
 
 // Global variables
 var player;
-var platform1;
-var platform2;
-var platform3;
 var platforms = [];
 var count;
+
+// load images
+var playerImg = new Image();
+var platformImg = new Image();
+playerImg.src = "imgs/Batman.png";
+platformImg.src = "";
 
 // Platform class
 var Platform = function (x, y, height, width, color) {   
@@ -84,17 +112,26 @@ var Platform = function (x, y, height, width, color) {
     this.move = function () {
     
         // Check if platform crossed left side of screen
-        if (platform1.x + platform1.width < 0) {
-            makeNewPlatform();
+        if (this.x + this.width < 0) {
+            // Find the index of this platform
+            var index = platforms.indexOf(this);
+            
+            // Check for index out of bounds
+            if (index > -1) {
+                
+                // Remove this object from platforms array
+                platforms.splice(index, 1);
+            }
         }
-        
-        if (platform2.x + platform2.width < 0) {
-            makeNewPlatformTwo();
-        }
-        
-        if (platform3.x + platform3.width < 0) {
-            makeNewPlatformThree();
-        }
+		
+		// Check if platform crossed middle of screen
+		else if (this.x + this.width <= game.canvas.width / 1.25 &&
+				this.x + this.width >= game.canvas.width / 1.25 - 2 &&
+				platforms.length <= 2) {
+			// Push (add) a new platform to the bldgs array
+                 this.x += this.dx;
+				 platforms.push(makeNewPlatform());
+		}
         
         else this.x += this.dx;
 
@@ -111,33 +148,23 @@ var Platform = function (x, y, height, width, color) {
 
 // initializes a new game
 function startGame() {
-
+	
+    // pause the game
+    kbd.p = true;
+    
+	// TO-DO: Create constants for number values
+	platforms = [new Platform(100, 220, PLATFORM_HEIGHT + 5, PLATFORM_WIDTH * 3, "brown")];
+	
     // width, height, color, x, y
-    player = new Player(30, 30, "red", 10, 120);
-    
-    makeNewPlatform();
-    makeNewPlatformTwo();
-    makeNewPlatformThree();
-    
-    // debug:
-    //console.log(game.canvas.width + " " + game.canvas.height);
-    //console.log(platform1);
+	// TO-DO: Create constants for number values
+    player = new Player(30, 30, "red", 50, 120);
 }
 
 // generates a new Platform
 function makeNewPlatform (height) {
     // x, y, height, width, color
-    platform1 = new Platform(INIT_X, INIT_Y, PLATFORM_HEIGHT, PLATFORM_WIDTH, "green");
-};
-
-function makeNewPlatformTwo (height) {
-    // x, y, height, width, color
-    platform2 = new Platform(INIT_X + 230, INIT_Y - 50, PLATFORM_HEIGHT, PLATFORM_WIDTH, "brown");
-};
-
-function makeNewPlatformThree (height) {
-    // x, y, height, width, color
-    platform3 = new Platform(INIT_X + 430, INIT_Y, PLATFORM_HEIGHT, PLATFORM_WIDTH, "blue");
+	var PLAT_Y = rand(MIN_PLAT_Y, MAX_PLAT_Y);
+    return new Platform(INIT_X, PLAT_Y, PLATFORM_HEIGHT, PLATFORM_WIDTH, "green");
 };
 
 // Player class
@@ -149,15 +176,16 @@ function Player(width, height, color, x, y) {
     this.height = height;
     //this.speedX = 0;
     this.speedY = 0;
-    this.gravity = 7;
+    this.gravity = 5;
     this.x = x;
     this.y = y;
     
     // Draw the player to screen
     this.update = function() {
         ctx = game.context;
-        ctx.fillStyle = color;
+		ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.drawImage(playerImg, this.x, this.y, this.width, this.height);
     }
     
     // Repositions the player for this frame
@@ -166,7 +194,7 @@ function Player(width, height, color, x, y) {
         // Speed without pressing keys
         this.speedX = 0;
         this.speedY = 0;
-
+		
         // Check if jump button is pressed
         if (kbd.up && this.landed) {
             this.jumping = true;
@@ -179,7 +207,7 @@ function Player(width, height, color, x, y) {
         }
         
         // reset jump if we're at top of jump
-        if (this.jumpCounter === 5) {
+        if (this.jumpCounter === 7) {
             this.jumping = false;
             this.jumpCounter = 0;
         }
@@ -193,84 +221,100 @@ function Player(width, height, color, x, y) {
     
     // Checks collisions between player and ground/platforms
     this.collisionDetect = function() {
+		
+		this.landed = false;
         
-        // this.x >= platform1.x - this.width
-        // this.x <= platform1.x + platform1.width
-        // this.x + this.width >= platform1.x
-        // this.x + this.width <= platform1.x + this.width
-        
-        // check for platform contact
-        if (this.x >= platform1.x - this.width
-            && this.x <= platform1.x + platform1.width) {
-            this.y = platform1.y - this.height;
-            this.landed = true;
-        }
-        
-        if (this.x >= platform2.x - this.width
-            && this.x <= platform2.x + platform2.width) {
-            this.y = platform2.y - this.height;
-            this.landed = true;
-        }
-        
-        if (this.x >= platform3.x - this.width
-            && this.x <= platform3.x + platform3.width) {
-            this.y = platform3.y - this.height;
-            this.landed = true;
-        }
+		for (var i = 0; i < platforms.length; i++) {
+			var platform = platforms[i];
+			// check for platform contact
+			if (this.x >= platforms[i].x - this.width
+				&& this.x <= platforms[i].x + platforms[i].width
+				&& this.y >= platforms[i].y - this.height
+				&& this.y <= platforms[i].y) {
+				
+				if (this.y + this.height <= platform.y + platform.height) {
+					this.y = platforms[i].y - this.height;
+					this.landed = true;
+				}
+				
+				// Platform pushes player
+				else if (this.x + this.width <= platform.x + PLATFORM_SPEED) {
+					// Reset game
+					if (this.x <= 0) {
+						startGame();
+					}
+					
+					this.jumping = false;
+					this.x = platform.x - this.width;
+				}
+				
+				else {
+					this.jumping = false;
+					this.y = platform.y + platform.height;
+				}
+				
+			}			
+		}
         
         // check for colliding with bottom of screen
-        else if (this.y >= game.canvas.height - this.height) {
+        if (this.y >= game.canvas.height - this.height) {
             this.y = game.canvas.height - this.height;
-            this.landed = true;
+            startGame();
         }
         
-        else {
+        /*else {
             this.landed = false;
-        }
+        }*/
     }
 }
 
 // Called multiple times
-function updateGameArea() {
-    /*
-    var x, y;
-    
-    for (i = 0; i < platforms.length; i += 1) {
-        if (player.crashWith(platform1[i])) {
-            game.stop();
-            return;
-        } 
-    }
-    game.clear();
-    game.frameNo += 1;
-    if (game.frameNo == 1 || everyinterval(150)) {
-        x = game.canvas.width;
-        y = game.canvas.height - 200
-        //todo
-        //platform1.push(new Player(10, 200, "green", x, y));
-    }
-    for (i = 0; i < platforms.length; i++) {
-        platform1[i].x += -1;
-        platform1[i].update();
-    }
-    */
-    
+function updateGameArea() {    
 
     // Clears screen of previous frames
     game.clear();
+	
+	// Refactor using functions
+	if (kbd.p) {
+		console.log("Paused");
+		kbd.p = false;
+		
+		clearInterval(game.interval);
+		
+		// wait for pause button to be pressed again
+		var w = setInterval (function() {
+			if (kbd.up || kbd.p) {
+				kbd.up = kbd.p = false;
+				clearInterval(w);
+				game.interval = setInterval (updateGameArea, 20);
+				console.log("Unpaused");
+			}
+		}, 20);
+	}
     
     // Handle collision detection
-    player.collisionDetect();
+    //player.collisionDetect();
 
     // Move stuff
     player.move();
-    platform1.move();
-    platform2.move();
-    platform3.move();
+    
+	for (var i = 0; i < platforms.length; i++) {
+        platforms[i].move();
+	}
 
     // Draw images
     player.update();
-    platform1.update();
-    platform2.update();
-    platform3.update();
+
+	for (var i = 0; i < platforms.length; i++) {
+        platforms[i].update();
+	}
 }
+
+// Generates a random integer between two bounds
+function rand(lo, hi) {
+    return Math.floor(Math.random() * (hi - lo)) + lo;
+}
+
+
+// was running into problems with body onload, so I added this:   -greg
+startGame();
